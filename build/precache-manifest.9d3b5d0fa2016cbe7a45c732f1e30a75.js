5c4abac2571241990c5dbb55ce2971418d882272
JamesK-Projects <63878760+JamesK-Projects@users.noreply.github.com>
2020-12-04 12:52:33 -0700

    deployed with vercel

---
+++ build/precache-manifest.9d3b5d0fa2016cbe7a45c732f1e30a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abab0135a66f9d19a5b29ddb90f4f1b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f57218e474f8597971",
+    "url": "/static/css/main.2a108850.chunk.css"
+  },
+  {
+    "revision": "42b3358f51238a1a5567",
+    "url": "/static/js/2.fc76880c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc76880c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f57218e474f8597971",
+    "url": "/static/js/main.6b6383e9.chunk.js"
+  },
+  {
+    "revision": "5e76467357d55565e73a",
+    "url": "/static/js/runtime-main.93766b80.js"
+  }
+]);
